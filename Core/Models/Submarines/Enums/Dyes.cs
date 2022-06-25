b72d291dc21abcede7f0d3eaa55797f578d4ae80
Mike Obermiller <michaeljobermiller@gmail.com>
2022-06-25 19:49:38 -0400

    Parts CRUD

---
+++ Core/Models/Submarines/Enums/Dyes.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Models.Submarines.Enums
+{
+    public enum Dyes
+    {
+        None = 0,
+        SootBlackDye = 1
+    }
+}
---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Core.Models.Submarines.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options)
+        : base(options) { }
+
+    public DbSet<Part> Parts { get; set; }
+}
